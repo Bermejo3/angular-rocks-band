0ba59d4436ad87ed9b0a4b9f1dcd216a58923bf0
Bermejo3 <alberto.bermejo.corredor@gmail.com>
2021-05-21 14:36:26 +0200

    band model

---
+++ src/app/models/band.ts
@@ -0,0 +1,15 @@
+export class Band {
+    constructor(
+        public id: number,
+        public name: string,
+        public website: string,
+        public description: string,
+        public video: string,
+        public isActive: boolean,
+        public year: number,
+        public picture: string,
+        public logo: string
+    ){
+
+    }
+}
